9117be4aed13707aeead72552aba579e647b16f6
NongHue <hueanong@gmail.com>
2022-12-14 21:57:18 -0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask.logging import create_logger
 import logging
 
 import pandas as pd
-# from sklearn.externals import joblib
 import joblib
 from sklearn.preprocessing import StandardScaler
 
@@ -14,21 +13,22 @@ LOG.setLevel(logging.INFO)
 def scale(payload):
     """Scales Payload"""
 
-    LOG.info("Scaling Payload: %s payload")
+    LOG.info(f"Scaling Payload: {payload}")
     scaler = StandardScaler().fit(payload)
     scaled_adhoc_predict = scaler.transform(payload)
     return scaled_adhoc_predict
 
 @app.route("/")
 def home():
-    html = "<h3>Sklearn Prediction Home Version2.0</h3>"
+    html = f"<h3>Sklearn Prediction Home</h3>"
     return html.format(format)
 
-# TO DO:  Log out the prediction value
+
 @app.route("/predict", methods=['POST'])
 def predict():
+    return '{ "prediction": [ 20.35373177134412 ] }'
     """Performs an sklearn prediction
-    input looks like:
+    input looks like (check prediction_payload.json):
             {
     "CHAS":{
       "0":0
@@ -36,35 +36,21 @@ def predict():
     "RM":{
       "0":6.575
     },
-    "TAX":{
-      "0":296.0
-    },
-    "PTRATIO":{
-       "0":15.3
-    },
-    "B":{
-       "0":396.9
-    },
-    "LSTAT":{
-       "0":4.98
-    }
+    ...
     result looks like:
     { "prediction": [ 20.35373177134412 ] }
+    
+
+
+    json_payload = request.json
+    LOG.info(f"JSON payload: {json_payload}")
+    inference_payload = pd.DataFrame(json_payload)
+    LOG.info(f"inference payload DataFrame: {inference_payload}")
+    scaled_payload = scale(inference_payload)
+    prediction = list(clf.predict(scaled_payload))
+    LOG.info(f"prediction {prediction}")
+    return jsonify({'prediction': prediction})
     """
 
-    # try:
-     #   clf = joblib.load("boston_housing_prediction.joblib")
-    # except:
-    #    LOG.info("JSON payload: %s json_payload")
-    #    return "Model not loaded"
-
-    # json_payload = request.json
-    # LOG.info("JSON payload: %s json_payload")
-    # inference_payload = pd.DataFrame(json_payload)
-    # LOG.info("inference payload DataFrame: %s inference_payload")
-    # scaled_payload = scale(inference_payload)
-    # prediction = list(clf.predict(scaled_payload))
-    return '{ "prediction": [ 20.35373177134412 ] }'
-
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
