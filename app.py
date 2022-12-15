52743f171a41e015cf57c9a720f7dc12767738ec
NongHue <hueanong@gmail.com>
2022-12-14 21:55:31 -0800

    Update app.py

--- app.py
+++ app.py
@@ -52,16 +52,16 @@ def predict():
     { "prediction": [ 20.35373177134412 ] }
     """
 
-    try:
-        clf = joblib.load("boston_housing_prediction.joblib")
-    except:
-        LOG.info("JSON payload: %s json_payload")
-        return "Model not loaded"
+    # try:
+     #   clf = joblib.load("boston_housing_prediction.joblib")
+    # except:
+    #    LOG.info("JSON payload: %s json_payload")
+    #    return "Model not loaded"
 
-    json_payload = request.json
-    LOG.info("JSON payload: %s json_payload")
-    inference_payload = pd.DataFrame(json_payload)
-    LOG.info("inference payload DataFrame: %s inference_payload")
+    # json_payload = request.json
+    # LOG.info("JSON payload: %s json_payload")
+    # inference_payload = pd.DataFrame(json_payload)
+    # LOG.info("inference payload DataFrame: %s inference_payload")
     # scaled_payload = scale(inference_payload)
     # prediction = list(clf.predict(scaled_payload))
     return '{ "prediction": [ 20.35373177134412 ] }'
